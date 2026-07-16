f7e266232eda9d1760240c52986a0c392f7b76b0
Kangjiwoong <woong020477@gmail.com>
2026-07-16 10:00:46 +0900

    Fix Backend URL

--- assets/config.js
+++ assets/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   // 실제 백엔드 API 주소
-  API_BASE_URL: 'https://backend-qthh.onrender.com',
+  API_BASE_URL: 'https://backend-qthh.onrender.com/api',
 
   // Kakao Developers에서 발급받은 JavaScript 키
   KAKAO_MAP_JS_KEY: '542e2dc4a4eafbef7bd34f3b0d36f469',
